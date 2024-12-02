b661b874d27ccff7e0ba08fd4bcddd4609cf6d4d
Oluwatofunmi Alugbin <oluwatofunmialugbin@gmail.com>
2024-12-02 14:03:21 +0100

    third commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// String data type
+// Number data
+//Boolean
+
+`My name is Tofunmi`
+30
+true
+let street = "No 5 Honeymoon"
+let age = 30
+let isCustomer = true
+
+300
+"rice and beans"
+let price = 300
+let food = "rice and beans"
+
+console.log(street);
+console.log(age);
+console.log(isCustomer);
+console.log(price);
+console.log(food);
+
+// addition
+let sum = 100 + 100
+console.log(sum);
+
+// multiplication
+let multiplicationResult = 10 * 10
+console.log(multiplicationResult);
+
